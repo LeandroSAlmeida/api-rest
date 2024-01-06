f9ed9c3e6a167097a4ec5d7d20a275a19eaba96c
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-01-06 05:25:16 -0300

    test: database seeding for test

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Leandro', '144.222.332-22', 20000.00,'2002-04-10', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Ana Maria', '333.244.222-21', 5000.00,'2000-04-11', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Daniel', '444.334.111-22', 4000.00,'1994-10-02', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Rosana', '221.443.552-10', 7000.00,'1990-12-03', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Michel', '114.322.177-12', 9000.00,'1999-11-03', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Amanda', '222.334.345-30', 2000.00,'1994-03-08', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('João', '876.375.943-01', 66000.00,'1991-09-18', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Natalia', '941.434.188-88', 1800.00,'2002-03-18', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Lucas', '783.334.001-29', 2000.00,'2000-10-10', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Laura', '777.888.777-55', 20000.00,'2002-07-04', 0);

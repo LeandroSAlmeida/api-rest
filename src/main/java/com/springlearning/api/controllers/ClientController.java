5380aa00c0afc186ba231a5f86905ce8af1ee3a0
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-01-06 06:00:35 -0300

    feat: method findAll

--- src/main/java/com/springlearning/api/controllers/ClientController.java
+++ src/main/java/com/springlearning/api/controllers/ClientController.java
@@ -3,19 +3,25 @@ package com.springlearning.api.controllers;
 import com.springlearning.api.dto.ClientDTO;
 import com.springlearning.api.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/client")
+@RequestMapping(value = "/clients")
 public class ClientController {
     @Autowired
     private ClientService service;
     @GetMapping(value = "/{id}")
-    private ClientDTO findById(@PathVariable Long id){
+    public ClientDTO findById(@PathVariable Long id){
     return service.findById(id);
+    }
+    @GetMapping
+    public Page<ClientDTO> findByAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 
 }
--- src/main/java/com/springlearning/api/services/ClientService.java
+++ src/main/java/com/springlearning/api/services/ClientService.java
@@ -3,16 +3,25 @@ package com.springlearning.api.services;
 import com.springlearning.api.domain.Client;
 import com.springlearning.api.dto.ClientDTO;
 import com.springlearning.api.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
+    @Autowired
     private ClientRepository repository;
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id){
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 
 }
